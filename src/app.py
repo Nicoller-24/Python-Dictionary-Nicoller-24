4ca31a242af0b57906c4136345a10765a7f21ef0
Nicolle Rodríguez Laytón <166450090+Nicoller-24@users.noreply.github.com>
2025-01-29 00:18:26 +0000

    API lenguajes finalzada

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Language
 #from models import Person
 
 app = Flask(__name__)
@@ -44,6 +44,38 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/languages', methods=['GET'])
+def get_languajes():
+    all_languages = list(Language.query.all())
+    results = list(map(lambda language: language.serialize(), all_languages))
+    return jsonify(results), 200
+
+
+@app.route("/language/create", methods=["POST"])
+def create_language():
+    body = request.get_json()
+
+    if "name" not in body:
+        return jsonify({"msg": "Faltan datos requeridos"}), 400
+
+    language_name = body["name"].upper()
+
+    existing_language = Language.query.filter_by(name=language_name).first()
+    if existing_language:
+        return jsonify({"msg": "El lenguaje ya existe"}), 409
+
+    language = Language(name=language_name)
+    db.session.add(language)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Lenguaje creado",
+        "result": language.serialize()
+    }
+    return jsonify(response_body), 201
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
