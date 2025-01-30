8a7318aefab9aaa64a99ad26a9cd3308e69298cc
Nicolle Rodríguez Laytón <166450090+Nicoller-24@users.noreply.github.com>
2025-01-30 23:03:35 +0000

    API finalizada

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User, Language
+from models import db, User, Language, Word
 #from models import Person
 
 app = Flask(__name__)
@@ -75,6 +75,110 @@ def create_language():
         "result": language.serialize()
     }
     return jsonify(response_body), 201
+
+@app.route('/words', methods=['GET'])
+def get_words():
+    all_words = list(Word.query.all())
+    results = list(map(lambda words: words.serialize(), all_words))
+    return jsonify(results), 200
+
+
+@app.route("/word/create", methods=["POST"])
+def create_word():
+    body = request.get_json()
+
+    if "word" not in body or "definition" not in body or "language_id" not in body:
+        return jsonify({"msg": "Faltan datos requeridos"}), 400
+
+
+    word = body["word"].upper()
+ 
+    existing_word = Word.query.filter_by(word=word).first()
+    if existing_word:
+        return jsonify({"msg": "La palabra ya existe"}), 409
+
+    language = Language.query.get(body["language_id"])
+    if not language:
+        return jsonify({"msg": "El lenguaje especificado no existe"}), 400
+
+    new_word = Word(
+        word=word,
+        definition=body["definition"],
+        language_id=body["language_id"]
+    )
+
+    db.session.add(new_word)
+    db.session.commit()
+
+    response_body = {
+        "msg": "Palabra creada",
+        "result": new_word.serialize()
+    }
+    return jsonify(response_body), 201
+
+
+@app.route('/words/language/<int:language_id>', methods=['GET'])
+def get_words_of_language(language_id):
+    language = Language.query.get(language_id)
+    if not language:
+        return jsonify({"msg": "El lenguaje especificado no existe"}), 404
+
+    words_language = Word.query.filter_by(language_id=language_id).all()
+
+    if not words_language:
+        return jsonify({"msg": "Aún no hay palabras registradas para este lenguaje"}), 200
+
+    response_data = [word.serialize() for word in words_language]
+
+    return jsonify(response_data), 200
+
+@app.route('/word/<string:language>/<string:word>', methods=['GET'])
+def get_word_by_language(language , word):
+    language_in_db = Language.query.filter_by(name=language.upper()).first()
+
+    if not language_in_db:
+       return jsonify({"msg": "El lenguaje especificado no existe"}), 404
+    
+    word_in_db = Word.query.filter_by(word=word.upper(), language_id= language_in_db.id).first()
+
+    if not word_in_db:
+       return jsonify({"msg": "La palabra especificada no existe"}), 404
+
+
+    return jsonify(word_in_db.serialize()), 200
+
+@app.route('/word/<int:word_id>', methods=['DELETE'])
+def delete_word(word_id):
+    word_to_delete = Word.query.get(word_id)
+    if word_to_delete:
+        db.session.delete(word_to_delete)
+        db.session.commit()
+        response_body = {"msg": "Se eliminó correctamente"}
+    else:
+        response_body = {"msg": "No se encontró la palabra"}
+    return jsonify(response_body), 200
+
+@app.route('/word/<int:word_id>', methods=['PUT'])
+def update_word(word_id):
+    body = request.get_json()
+    word_to_edit = Word.query.filter_by(id=word_id).first()
+    if word_to_edit is None:
+        return jsonify({"error": "Palabra no encontrada"}), 404
+
+    if "word" in body:
+        word_to_edit.word = body["word"]
+    if "definition" in body:
+        word_to_edit.definition = body["definition"]
+    if "language_id" in body:
+        language = Language.query.filter_by(id=body["language_id"]).first()
+        if language is None:
+            return jsonify({"error": "Lenguaje no encontrado"}), 404
+        else: 
+            word_to_edit.language_id = body["language_id"]
+
+    db.session.commit()
+
+    return jsonify({"msg": "Se actualizado con éxito", "word": word_to_edit.serialize()}), 200
 
 
 # this only runs if `$ python src/app.py` is executed
